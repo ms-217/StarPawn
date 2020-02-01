a9a1d6582b41f9cf04aa5e6bb285cf58353bd5b4
Mason Schmidgall <ms-217@users.noreply.github.com>
2020-01-31 23:52:49 -0600

    Add background and basic item functionality

---
+++ StarPawn/Assets/Scripts/BuyItems.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BuyItems : MonoBehaviour
+{
+    public GameObject[] itemsArr;
+    public GameObject currentItem;
+    int rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rand = (new System.Random()).Next(0, 2);
+
+        Item = gameObject.Getcomponet.itemsArr[rand];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
