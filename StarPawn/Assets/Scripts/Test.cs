42093f045f3c69202437504de8495665291cc8da
sausymeme <aaronmste@gmail.com>
2020-02-01 15:49:34 -0600

    Revert "DID EVERYTHING"
    This reverts commit dcac388076b2f203aff8259584f17b24d5864c3c.

--- StarPawn/Assets/Scripts/Test.cs
+++ StarPawn/Assets/Scripts/Test.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomerSpawn : MonoBehaviour
+public class Test : MonoBehaviour
 {
+    
+    public GameObject[] itemsArr;
+    public GameObject currentItem;
+    int rand;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rand = (new System.Random()).Next(0, 1);
+
+       // Item = gameObject.Getcomponet.itemsArr[rand];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
