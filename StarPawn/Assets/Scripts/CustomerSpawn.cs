2b20417e6f8b3fd3592eb9f13d766cc42d60f32e
sausymeme <aaronmste@gmail.com>
2020-02-01 15:51:30 -0600

    Revert "Revert "DID EVERYTHING""
    This reverts commit 42093f045f3c69202437504de8495665291cc8da.

--- StarPawn/Assets/Scripts/CustomerSpawn.cs
+++ StarPawn/Assets/Scripts/CustomerSpawn.cs
@@ -2,25 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class CustomerSpawn : MonoBehaviour
 {
-    
-    public GameObject[] itemsArr;
-    public GameObject currentItem;
-    int rand;
-
     // Start is called before the first frame update
     void Start()
     {
-        rand = (new System.Random()).Next(0, 1);
-
-       // Item = gameObject.Getcomponet.itemsArr[rand];
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
-
---
+++ StarPawn/Assets/Scripts/UIRight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRight : MonoBehaviour
+{
+
+    public float speed = 1;
+    public float UITimer = 1;
+    public GameObject UIPose;
+    Transform UIPos;
+    public bool display = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIPos = UIPose.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (display == false)
+            UITimer -= Time.deltaTime;
+
+        if (UITimer <= 0 && display == false)
+        {
+            display = true;
+        }
+
+        if (UITimer > 0)
+            transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
