5bdaf72693c8030f8165ba05bd9e012a907e55b9
Mason Schmidgall <ms-217@users.noreply.github.com>
2020-02-01 11:42:53 -0600

    Finalize fields in BuyItems

--- StarPawn/Assets/Scripts/BuyItems.cs
+++ StarPawn/Assets/Scripts/BuyItems.cs
@@ -7,6 +7,13 @@ public class BuyItems : MonoBehaviour
 {
     public GameObject[] itemsArr;
     public GameObject currentItem;
+    public string Name;
+    public string Rarity;
+    public string ItemDescription;
+    public int Risk;
+    public int RepairCost;
+    public int Price;
+
     int rand;
 
     // Start is called before the first frame update
@@ -15,8 +22,12 @@ public class BuyItems : MonoBehaviour
         rand = (new System.Random()).Next(0, 2);
 
         currentItem = itemsArr[rand];
-
+        ItemDescription = "This is a Crystal";
+        Risk = "";
+        Name = "";
+        Rarity = "";
         
+
     }
 
     // Update is called once per frame
