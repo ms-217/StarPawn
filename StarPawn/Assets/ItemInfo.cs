e84bfa66caa4e34eb173b3bca1fbb983af77c978
sausymeme <aaronmste@gmail.com>
2020-02-01 15:49:23 -0600

    Revert "merge"
    This reverts commit 7966a5b5ce104db39b433a858a0948b99b2ec171, reversing
    changes made to dcac388076b2f203aff8259584f17b24d5864c3c.

--- StarPawn/Assets/ItemInfo.cs
+++ StarPawn/Assets/ItemInfo.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DisplayDescription : MonoBehaviour
+
+public class ItemInfo : MonoBehaviour
 {
     public Text description;
-    public GameObject CurrentItemOnCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
         description = GetComponent<Text>();
-        CurrentItemOnCanvas = GameObject.FindGameObjectWithTag("Item");
-        
-
-
+        description.text = "This is a code test";
     }
 
     // Update is called once per frame
     void Update()
     {
-        description.text = CurrentItemOnCanvas.GetComponent<BuyItems>().ItemDescription;
+        
     }
 }
